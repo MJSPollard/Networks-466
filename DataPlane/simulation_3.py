1dd686f049f61aca53c2b6c898be254fd8500b5e
MJSPollard <mpollard@wolfssl.com>
2018-10-31 17:24:06 -0600

    updated file structure

--- DataPlane/simulation_3.py
+++ DataPlane/simulation_3.py
@@ -3,8 +3,8 @@ Created on Oct 12, 2016
 
 @author: mwittie
 '''
-import network
-import link
+import network_3
+import link_3
 import threading
 from time import sleep
 
@@ -16,21 +16,21 @@ if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
     
     #create network nodes
-    client = network.Host(1)
+    client = network_3.Host(1)
     object_L.append(client)
-    server = network.Host(2)
+    server = network_3.Host(2)
     object_L.append(server)
-    router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    router_a = network_3.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_a)
     
     #create a Link Layer to keep track of links between network nodes
-    link_layer = link.LinkLayer()
+    link_layer = link_3.LinkLayer()
     object_L.append(link_layer)
     
     #add all the links
     #link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
-    link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link.Link(router_a, 0, server, 0, 50))
+    link_layer.add_link(link_3.Link(client, 0, router_a, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 0, server, 0, 50))
     
     
     #start all the objects
