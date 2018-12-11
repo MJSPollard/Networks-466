b0594823c5686b8e3e177a0ae7bc5f7e6a756b7f
Dillon Tice <dtice@localhost.localdomain>
2018-12-10 19:30:23 -0700

    .

--- MPLS/link_3.py
+++ MPLS/link_3.py
@@ -89,12 +89,11 @@ class Link:
                           ' - seconds until the next available time %f\n' \
                           ' - queue size %d' \
                           % (self, pkt_S, node_a, node_a_intf, node_b, node_b_intf, intf_a.next_avail_time - time.time(), intf_a.out_queue.qsize()))
-
-                print("\n #-------- Queue for %s--------# \n" % self.node_1)
-                for pkt in intf_a.out_queue.queue:
-                    print("Packet with Priority: ",pkt[-1])
-                    print('\n-------------------\n')
-                print('\n#-----------------#\n')
+                if intf_a.out_queue.qsize() != 0:
+                    print("\n #-------- Queue for %s--------# \n" % self.node_1)
+                    for pkt in intf_a.out_queue.queue:
+                        print("Packet with Priority: ",pkt[-1])
+                    print('\n#-----------------#\n')
 
                 # uncomment the lines below to see waiting time until next transmission
 #                 else:
